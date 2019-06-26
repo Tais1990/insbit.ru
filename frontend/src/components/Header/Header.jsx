7ec2887306b99e9b3ea6f0f16b8290846ed105d1
Tais1990 <tais_kat@list.ru>
2019-06-27 01:49:23 +0500

    Header and header-expansion

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -11,18 +11,23 @@ class Header extends React.Component {
     }
     render(props, state) {
         let logo = PARH_RESOURCE + 'img/webucator.png';
-        return <header className = 'header'>
-            <div className = 'header header__left'>             
-                <Button title = 'ВОЙТИ' type = 'link' link = '/'/>    
-            </div>         
-            <div  className = 'header header__center'>
-                <Img src = {logo} width = {211} height = {52} link="/"/>
-            </div>
-            <div  className = 'header header__right contacts'>
-                <div className = 'contacts contacts__tel'>+7-877-932-8228</div>
-                <Button title = 'НАШИ КОНТАКТЫ' type = 'button' color='light' link = '/'/>
-            </div>
-        </header>
+        return <div>
+            <header>
+                <div  className = 'header'>
+                    <div className = 'header header__left'>             
+                        <Button title = 'ВОЙТИ' type = 'link' link = '/'/>    
+                    </div>         
+                    <div  className = 'header header__center'>
+                        <Img src = {logo} width = {211} height = {52} link="/"/>
+                    </div>
+                    <div  className = 'header header__right contacts'>
+                        <div className = 'contacts contacts__tel'>+7-877-932-8228</div>
+                        <Button title = 'НАШИ КОНТАКТЫ' type = 'button' color='light' link = '/'/>
+                    </div>
+                </div>
+            </header>
+            <div className='header-expansion'/>
+        </div> 
     }
 }
 export default Header;
