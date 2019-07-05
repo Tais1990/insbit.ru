e5aad2bd5eb13e4798814a070ede476969b18009
Tais1990 <tais_kat@list.ru>
2019-07-05 22:07:49 +0500

    скрипт инициализации

--- db/manager.py
+++ db/manager.py
@@ -2,35 +2,6 @@ import peewee
 from db.dbhandle import *
 from db.models import *
 
-def function():
-    print('test')
-
-def test():
-    return {"id": "1", "name": "321"}
-
-def find_all_categories():
-    return Category.select()
-
-
-
-
-def test1():
-    try:
-        dbhandle.connect()
-        #Category.create_table()
-        products = find_all_categories()
-        product_data = []
-        for product in products:
-            product_data.append({
-                'id': product.id,
-                'name': product.name
-            })
-        #print(products)
-        return product_data
-    except peewee.InternalError as px:
-        print(str(px))
-
-
 # Работа с курсом непосредственное
 # создание таблицы
 def coursesCreateTable():
--- views.py
+++ views.py
@@ -33,18 +33,7 @@ class SiteHandler:
             'nameTrainingPrograms': request.match_info['nameTrainingPrograms'],
             'nameCourse': request.match_info['nameCourse']}
 
-    async def getAll(self, request: web.Request) -> Dict[str, str]:
-        
-        #manager.coursesAdd('code1', 
-        #    'Курс «Администрирование и устранение неполадок в Office 365»', 
-        #    'Этот трехдневный курс обучения под руководством инструктора, предназначенный для специалистов по информационным технологиям (ИТ), которые участвуют в администрировании, настройке, устранении неполадок и эксплуатации служб Office 365, включая их идентификационные данные, зависимости, требования и поддержку. технологии. Этот курс посвящен навыкам, необходимым для администрирования и устранения неполадок клиента Office 365 и ключевых служб Office 365, таких как Exchange Online, SharePoint Online и Skype для бизнеса. Он также охватывает функции безопасности и соответствия требованиям Office 365.', 
-        #    '10997B',
-        #    'Этот курс предназначен для ИТ-специалистов и системных администраторов, которые хотят узнать о методах администрирования и устранения неполадок в Office 365. Эти специалисты должны иметь как минимум трехлетний опыт работы в соответствующих областях: в идеале - в области системного администрирования, сетевого администрирования, и обмен сообщениями.',
-        #    '3 дня – 24 ак. часа',
-        #    ["Минимум два года опыта администрирования операционной системы Windows Server, включая Windows Server 2012 или новее.", "Минимум один год опыта работы с доменными службами Active Directory (AD DS)."],
-        #    ["Описывать службы Office 365", "Администрировать Office 365 с помощью графической среды и Windows PowerShell"],
-        #    '<ul><li>первый модуль</li><li>второй модуль</li></ul>')
-        #manager.coursesCreateTable()
+    async def getAll(self, request: web.Request) -> Dict[str, str]:        
         res = manager.coursesSelectAll()
         return web.json_response(res)
     
@@ -81,19 +70,9 @@ class SiteHandler:
 
     async def editCourse(self, request: web.Request) -> web.Response:
         form = await request.json()
-        
-        print(form.get('code'))
-        #manager.coursesEdit()
-       
-        #raw_data = form['file'].file.read()
-        #executor = request.app['executor']
-        #r = self._loop.run_in_executor
-        #raw_data = await r(executor, predict, raw_data)
-        #raw_data = '{"code"}'
         manager.coursesEditSmall(form.get('code'), form.get('name'))
         headers = {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': 'http://localhost:8081'
             }
         return web.Response(status=200, headers=headers)
-        #return web.Response(headers=headers)
