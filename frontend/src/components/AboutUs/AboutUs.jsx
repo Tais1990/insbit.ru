60d5c40591b6b108005a6b07ebcf103b820ae272
Tais1990 <tais_kat@list.ru>
2019-08-01 09:31:17 +0500

    https://git.night.army/Kypc.pro/insbit.ru/issues/38  Создать страницу политики отмены

--- frontend/src/components/AboutUs/AboutUs.jsx
+++ frontend/src/components/AboutUs/AboutUs.jsx
@@ -13,7 +13,6 @@ class AboutUs extends React.Component {
 
     render(props, state) {
         let {type} = this.props; 
-        console.log(type);
         if (type == 'about')
         {       
             return <div className = 'about-us about-us__about'>
@@ -41,7 +40,7 @@ class AboutUs extends React.Component {
             return <div className = 'about-us about-us__privacy-policy'>
                 <SectionBlock title = 'Политика конфиденциальности' width = 'narrow-width'/>  
                 <SectionBlock  color='grey'>
-                    <div className = 'container'>
+                    <div className = 'container container__privacy-policy'>
                         <h3>Какую информацию мы собираем?</h3>
                         <p>Мы собираем информацию о Вас, когда Вы регистрируетесь на нашем сайте, размещаете заказ или заполняете форму. При заказе
                         или регистрации на нашем сайте, при необходимости, Вас могут попросить ввести: имя, адрес электронной почты, почтовый адрес,
@@ -99,6 +98,31 @@ class AboutUs extends React.Component {
                 </SectionBlock>
             </div>
         }
+        if (type == 'cancelation') {
+            return <div className = 'about-us about-us__cancelation'>
+                <SectionBlock title = 'Политика отмены' width = 'narrow-width'/>  
+                <SectionBlock  color='grey'>
+                    <div className = 'container container__cancelation'>
+                        <div className='panel-header'>Online Курсы</div>
+                        <div className='panel-body'>
+                            <b>Если мы отменяем курс,</b> что случается редко, учащиеся, зарегистрированные на это обучение, получают выбор между:
+                            <ol>
+                                <li>Получение полного возврата средств или</li>
+                                <li>Перепланирование на другие даты, если таковые предлагаются.</li>
+                            </ol>
+                            <b>Если студент отменяет,</b> то плата за <b>отмену</b> зависит от суммы уведомления:
+                            <ul>
+                                <li><b>14 и более дней:</b> плата за отмену не взимается (полный возврат средств).</li>
+                                <li><b>13 дней или меньше:</b> возврат не производится. Если возможно, мы предложим Вам перенести обучение на следующую дату. 
+                                Перепланирование не гарантируется и доступно только в том случае, если дата указана в нашем расписании.</li>
+                            </ul>
+                            <b>Обратите внимание: </b> если курс, на который вы регистрируетесь, начинается через 2 недели с сегодняшнего дня, 
+                            возмещение не будет произведено, если вы отмените его.
+                        </div>
+                    </div>
+                </SectionBlock>
+            </div>
+        }
         else 
         {
             return <div>
@@ -108,6 +132,6 @@ class AboutUs extends React.Component {
     }
 }
 AboutUs.propTypes = {
-	type: PropTypes.oneOf(['about', 'privacy-policy'])
+	type: PropTypes.oneOf(['about', 'privacy-policy', 'cancelation'])
 };
 export default AboutUs;
